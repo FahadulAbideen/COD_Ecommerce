e3d7f92c4b87852c91e54db133b6d18a699c0ddd
fAdi Devs <fahadulabideen@gmail.com>
2020-11-09 20:35:42 +0500

    Integration of template to Laravel Project

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,309 @@
+<!doctype html>
+<html lang="zxx">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title> @yield('title')</title>
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="{{asset('css/style-starter.css')}}">
+  <!-- Template CSS -->
+  <link href="//fonts.googleapis.com/css?family=Oswald:300,400,500,600&display=swap" rel="stylesheet">
+  <link href="//fonts.googleapis.com/css?family=Lato:300,300i,400,400i,700,900&display=swap" rel="stylesheet">
+  <!-- Template CSS -->
+</head>
+<body>
+    @section('header')
+    <header class="tophny-header">
+        <div class="container-fluid">
+          <div class="top-right-strip row">
+            <!--/left-->
+            <div class="top-hny-left-content col-lg-6 pl-lg-0">
+              <h6>Upto 30% off on All styles , <a href="#" target="_blank"> Click here for <span
+                    class="fa fa-hand-o-right hand-icon" aria-hidden="true"></span> <span class="hignlaite">More
+                    details</span></a></h6>
+            </div>
+            <!--//left-->
+            <!--/right-->
+            <ul class="top-hnt-right-content col-lg-6">
+
+              <li class="button-log usernhy">
+                <a class="btn-open" href="#">
+                  <span class="fa fa-user" aria-hidden="true"></span>
+                </a>
+              </li>
+              <li class="transmitvcart galssescart2 cart cart box_1">
+                <form action="#" method="post" class="last">
+                  <input type="hidden" name="cmd" value="_cart">
+                  <input type="hidden" name="display" value="1">
+                  <button class="top_transmitv_cart" type="submit" name="submit" value="">
+                    My Cart
+                    <span class="fa fa-shopping-cart"></span>
+                  </button>
+                </form>
+              </li>
+            </ul>
+            <!--//right-->
+            <div class="overlay-login text-left">
+              <button type="button" class="overlay-close1">
+                <i class="fa fa-times" aria-hidden="true"></i>
+              </button>
+              <div class="wrap">
+                <h5 class="text-center mb-4">Login Now</h5>
+                <div class="login-bghny p-md-5 p-4 mx-auto mw-100">
+                  <!--/login-form-->
+                  <form action="#" method="post">
+                    <div class="form-group">
+                      <p class="login-texthny mb-2">Email address</p>
+                      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                        placeholder="" required="">
+                      <small id="emailHelp" class="form-text text-muted">We'll never share your email
+                        with anyone else.</small>
+                    </div>
+                    <div class="form-group">
+                      <p class="login-texthny mb-2">Password</p>
+                      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="" required="">
+                    </div>
+                    <div class="form-check mb-2">
+                      <div class="userhny-check">
+                        <label class="check-remember container">
+                          <input type="checkbox" class="form-check"> <span class="checkmark"></span>
+                          <p class="privacy-policy">Remember me</p>
+                        </label>
+                        <div class="clearfix"></div>
+                      </div>
+                    </div>
+                    <button type="submit" class="submit-login btn mb-4">Sign In</button>
+
+                  </form>
+                  <!--//login-form-->
+                </div>
+                <!---->
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--/nav-->
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <div class="container-fluid serarc-fluid">
+            <a class="navbar-brand" href="/">
+              Spry<span class="lohny">S</span>tore</a>
+            <!-- if logo is image enable this   
+                    <a class="navbar-brand" href="#index.html">
+                      <img src="image-path" alt="Your logo" title="Your logo" style="height:35px;" />
+                    </a> -->
+            <!--/search-right-->
+            <div class="search-right">
+
+              <a href="#search" title="search"><span class="fa fa-search mr-2" aria-hidden="true"></span>
+                <span class="search-text">Search here</span></a>
+              <!-- search popup -->
+
+
+              <div id="search" class="pop-overlay">
+                <div class="popup">
+
+                  <form action="#" method="post" class="search-box">
+                    <input type="search" placeholder="Keyword" name="search" required="required" autofocus="">
+                    <button type="submit" class="btn">Search</button>
+                  </form>
+
+                </div>
+
+                <a class="close" href="#">×</a>
+              </div>
+              <!-- /search popup -->
+            </div>
+            <!--//search-right-->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            	<span class="navbar-toggler-icon fa fa-bars"> </span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                  <a class="nav-link" href="/">Home</a>
+                </li>
+                <li class="nav-item active">
+                  <a class="nav-link" href="/about">About</a>
+                </li>
+               
+                <li class="nav-item">
+                  <a class="nav-link" href="/about">What We Offer</a>
+                </li>
+               
+
+                <li class="nav-item">
+                  <a class="nav-link" href="/contact">Contact</a>
+                </li>
+              </ul>
+
+            </div>
+          </div>
+        </nav>
+        <!--//nav-->
+      </header>
+    @show
+    @yield('content')
+    @include('layout.footer')
+</body>
+</html>
+
+{{-- index --}}
+
+<script src="{{asset('js/jquery-3.3.1.min.js')}}"></script>
+<script src="{{asset('js/jquery-2.1.4.min.js')}}"></script>
+<!--/login-->
+<script>
+		$(document).ready(function () {
+			$(".button-log a").click(function () {
+				$(".overlay-login").fadeToggle(200);
+				$(this).toggleClass('btn-open').toggleClass('btn-close');
+			});
+		});
+		$('.overlay-close1').on('click', function () {
+			$(".overlay-login").fadeToggle(200);
+			$(".button-log a").toggleClass('btn-open').toggleClass('btn-close');
+			open = false;
+		});
+  </script>
+<!--//login-->
+<script>
+// optional
+		$('#customerhnyCarousel').carousel({
+				interval: 5000
+    });
+  </script>
+ <!-- cart-js -->
+ <script src="{{asset('js/minicart.jsminicart.js')}}"></script>
+ <script>
+     transmitv.render();
+
+     transmitv.cart.on('transmitv_checkout', function (evt) {
+         var items, len, i;
+
+         if (this.subtotal() > 0) {
+             items = this.items();
+
+             for (i = 0, len = items.length; i < len; i++) {}
+         }
+     });
+ </script>
+ <!-- //cart-js -->
+<!--pop-up-box-->
+<script src="{{asset('js/jquery.magnific-popup.js')}}"></script>
+<!--//pop-up-box-->
+<script>
+  $(document).ready(function () {
+    $('.popup-with-zoom-anim').magnificPopup({
+      type: 'inline',
+      fixedContentPos: false,
+      fixedBgPos: true,
+      overflowY: 'auto',
+      closeBtnInside: true,
+      preloader: false,
+      midClick: true,
+      removalDelay: 300,
+      mainClass: 'my-mfp-zoom-in'
+    });
+
+  });
+</script>
+<!--//search-bar-->
+<!-- disable body scroll which navbar is in active -->
+
+<script>
+  $(function () {
+    $('.navbar-toggler').click(function () {
+      $('body').toggleClass('noscroll');
+    })
+  });
+</script>
+<!-- disable body scroll which navbar is in active -->
+<script src="{{asset('js/bootstrap.min.js')}}"></script>
+
+{{-- about --}}
+
+<script src="{{asset('js/jquery-3.3.1.min.js')}}"></script>
+<!-- disable body scroll which navbar is in active -->
+<script>
+  $(function () {
+    $('.navbar-toggler').click(function () {
+      $('body').toggleClass('noscroll');
+    })
+  });
+</script>
+<!-- disable body scroll which navbar is in active -->
+<script src="{{asset('js/jquery-2.1.4.min.js')}}"></script>
+<!--/login-->
+<script>
+		$(document).ready(function () {
+			$(".button-log a").click(function () {
+				$(".overlay-login").fadeToggle(200);
+				$(this).toggleClass('btn-open').toggleClass('btn-close');
+			});
+		});
+		$('.overlay-close1').on('click', function () {
+			$(".overlay-login").fadeToggle(200);
+			$(".button-log a").toggleClass('btn-open').toggleClass('btn-close');
+			open = false;
+		});
+  </script>
+<!--//login-->
+<!-- //jQuery-Photo-filter-lightbox-portfolio-plugin -->
+<script src="{{asset('js/bootstrap.min.js')}}"></script>
+
+{{-- contact --}}
+
+<script src="{{asset('js/jquery-3.3.1.min.js')}}"></script>
+<script src="{{asset('js/jquery-2.1.4.min.js')}}"></script>
+<!--/login-->
+<script>
+		$(document).ready(function () {
+			$(".button-log a").click(function () {
+				$(".overlay-login").fadeToggle(200);
+				$(this).toggleClass('btn-open').toggleClass('btn-close');
+			});
+		});
+		$('.overlay-close1').on('click', function () {
+			$(".overlay-login").fadeToggle(200);
+			$(".button-log a").toggleClass('btn-open').toggleClass('btn-close');
+			open = false;
+		});
+  </script>
+<!--//login-->
+<script>
+// optional
+		$('#customerhnyCarousel').carousel({
+				interval: 5000
+    });
+  </script>
+ <!-- cart-js -->
+ <script src="{{asset('js/minicart.js')}}"></script>
+ <script>
+     transmitv.render();
+
+     transmitv.cart.on('transmitv_checkout', function (evt) {
+         var items, len, i;
+
+         if (this.subtotal() > 0) {
+             items = this.items();
+
+             for (i = 0, len = items.length; i < len; i++) {}
+         }
+     });
+ </script>
+ <!-- //cart-js -->
+
+<!-- disable body scroll which navbar is in active -->
+
+<script>
+  $(function () {
+    $('.navbar-toggler').click(function () {
+      $('body').toggleClass('noscroll');
+    })
+  });
+</script>
+<!-- disable body scroll which navbar is in active -->
+<script src="{{asset('js/bootstrap.min.js')}}"></script>
+
